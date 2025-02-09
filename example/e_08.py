6d1dba30e6cdb3cb8147a34dc84d3ce4d56bba91
rsfo <vlmr.rsfo@gmail.com>
2025-02-09 16:54:30 -0200

    Add 16th update

---
+++ example/e_08.py
@@ -0,0 +1,11 @@
+import os
+import logfire
+from dotenv import load_dotenv
+
+load_dotenv()
+
+LOGFIRE_TOKEN = os.getenv("LOGFIRE_TOKEN")
+
+logfire.configure(token=LOGFIRE_TOKEN)
+
+logfire.info('Hello, {name}!', name='World')
